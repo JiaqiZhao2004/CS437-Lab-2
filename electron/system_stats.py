798513f5e071e2ab33d7d64ff56938772b25b4ac
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2025-03-09 21:16:41 -0500

    try 6

--- electron/system_stats.py
+++ electron/system_stats.py
@@ -10,7 +10,7 @@ def get_cpu_temperature():
         return None
 
 def get_cpu_usage():
-    return psutil.cpu_percent(interval=1)
+    return psutil.cpu_percent(interval=0)
 
 def get_memory_usage():
     return psutil.virtual_memory().percent
